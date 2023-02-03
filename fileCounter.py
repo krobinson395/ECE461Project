8b2aa133c86028b7c69daa7a20dab100b82716d3
Kyle <robin489@purdue.edu>
2023-02-02 20:21:00 -0500

    Added gitPython support to lineCounter, additionally added run bash script to control overall proccesses

--- fileCounter.py
+++ fileCounter.py
@@ -1,15 +1,21 @@
 import os
 import csv
 import math
+import git
+import sys
 def main():
-    createClocFile('./repoDir', 'clocOutput')
+    print(str(sys.argv[1]))
+    gitURL = str(sys.argv[1])
+    cloneRepo(gitURL)
+    createClocFile('./tmpRepo', 'clocOutput')
     clocOut = readClocFile('clocOutput')
-    print('Comments: ' + str(clocOut[0]))
-    print('Code Lines: ' + str(clocOut[1]))
-    print('Total Lines: ' +  str(clocOut[2]))
+    #print('Comments: ' + str(clocOut[0]))
+    #print('Code Lines: ' + str(clocOut[1]))
+    #print('Total Lines: ' +  str(clocOut[2]))
     rampUp = calcRampUp(clocOut[0], clocOut[1])
-    print('Ramp Up: ' + str(rampUp))
-
+    #print('Ramp Up: ' + str(rampUp))
+    writeToFile('test.tmp', 'exampleGitHubURL', str(clocOut[0]), str(clocOut[1]), str(rampUp))
+    deleteRepo()
 def createClocFile(repoDir, outputFile):
 
     clocLoc = 'cloc/cloc'
@@ -41,7 +47,19 @@ def calcRampUp(docLines, codeLines):
     rampUp = math.tanh(1.5 * ratio / (math.log(codeLines, 100)))
     return(rampUp)
 
+def writeToFile(fileName, gitURL, docLines, codeLines, rampUp):
+    file = open(fileName, 'w')
+    file.write(gitURL + "\n")
+    file.write(codeLines + "\n")
+    file.write(docLines+ "\n")
+    file.write(rampUp)
+    file.close()
+def cloneRepo(gitURL):
+    git.Repo.clone_from(gitURL, './tmpRepo')
 
+def deleteRepo():
+    os.system('rm -r ./tmpRepo')
+    os.system('rmdir tmpRepo')
 
 if __name__ == "__main__":
     main()
