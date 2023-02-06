f8bea8cda0faa61934ffe2f125e9a25a3791c5c2
Kyle <robin489@purdue.edu>
2023-02-06 18:15:34 -0500

    Added correctness functionality to the fileCounter file

--- fileCounter.py
+++ fileCounter.py
@@ -4,17 +4,21 @@ import math
 import git
 import sys
 def main():
+    repoDir = './tmpRepo'
     print(str(sys.argv[1]))
     gitURL = str(sys.argv[1])
     cloneRepo(gitURL)
-    createClocFile('./tmpRepo', 'clocOutput')
+    createClocFile(repoDir, 'clocOutput')
     clocOut = readClocFile('clocOutput')
     #print('Comments: ' + str(clocOut[0]))
     #print('Code Lines: ' + str(clocOut[1]))
     #print('Total Lines: ' +  str(clocOut[2]))
     rampUp = calcRampUp(clocOut[0], clocOut[1])
+    findTestDirs(repoDir)
+    numTestLines = countLinesTest('testList', repoDir)
+    correctness  = 1 if numTestLines > 20000 else numTestLines / 20000
     #print('Ramp Up: ' + str(rampUp))
-    writeToFile('test.tmp', 'exampleGitHubURL', str(clocOut[0]), str(clocOut[1]), str(rampUp))
+    writeToFile('info.tmp', 'exampleGitHubURL', str(clocOut[0]), str(clocOut[1]), str(rampUp), str(correctness))
     deleteRepo()
 def createClocFile(repoDir, outputFile):
 
@@ -47,19 +51,43 @@ def calcRampUp(docLines, codeLines):
     rampUp = math.tanh(1.5 * ratio / (math.log(codeLines, 100)))
     return(rampUp)
 
-def writeToFile(fileName, gitURL, docLines, codeLines, rampUp):
+def writeToFile(fileName, gitURL, docLines, codeLines, rampUp, correctness):
     file = open(fileName, 'w')
     file.write(gitURL + "\n")
     file.write(codeLines + "\n")
     file.write(docLines+ "\n")
-    file.write(rampUp)
+    file.write(rampUp + "\n")
+    file.write(correctness + "\n")
     file.close()
 def cloneRepo(gitURL):
     git.Repo.clone_from(gitURL, './tmpRepo')
 
 def deleteRepo():
-    os.system('rm -r ./tmpRepo')
-    os.system('rmdir tmpRepo')
+    os.system('rm -rf ./tmpRepo')
+    #os.system('rmdir tmpRepo')
+
+def findTestDirs(repoDir):
+    cmd1 = "ls " + repoDir + " | grep test > testList"
+    cmd2 = "ls " + repoDir + " | grep Test >> testList"
+    os.system(cmd1)
+    os.system(cmd2)
+def countLinesTest(testFile, repoDir):
+    file = open(testFile, 'r')
+    testDirs = file.read().splitlines()
+    for testDir in testDirs:
+        fullDir = repoDir + '/' + testDir
+        #print(fullDir)
+        cmd = 'cloc/cloc --csv ' + fullDir + ' | tail -n 1 >> numTestLines'
+        #print(cmd)
+        os.system(cmd)
+    lineCount = 0
+    with open("./numTestLines") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        for row in csv_reader:
+            lineCount+= int(row[4])
+    os.system("rm ./numTestLines")
+    os.system("rm ./testList")
+    return(lineCount);
 
 if __name__ == "__main__":
     main()
