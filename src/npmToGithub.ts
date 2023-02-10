1051f56f6a8f12251a64c28678eedd0ee719c8ae
Jason H Bleasdell <jbleasde@eceprog3.ecn.purdue.edu>
2023-02-09 23:00:37 -0500

    edge case testing fixes for npmToGithub file

--- src/npmToGithub.ts
+++ src/npmToGithub.ts
@@ -1,22 +1,12 @@
 import * as fs from 'fs';
 import * as readline from 'readline';
 let {PythonShell} = require('python-shell'); 
-import dotenv from "dotenv"
+import * as dotenv from 'dotenv';
 
 dotenv.config()
 
 const {Octokit} = require("@octokit/core");
 const { request } = require("@octokit/request");
-
-var pyFile = 'pythonScript.py'
-
-let options = {
-    pythonOptions: ['-u'], // get print results in real-time
-    args: ['pythonOutput.txt']
-  };
-
-
-console.log("begin")
 
 //DELTE THE AUTH TOKEN BEFORE UPLOADING TO GITHUB
 const octokit = new Octokit ({
@@ -36,6 +26,7 @@ const logFile = "logFile.txt"
 
 const emptyString = "";
 
+
 const resp = async(line:string) => {
     let fullLine:string = line
     let replacement = "registry"
@@ -49,22 +40,29 @@ const resp = async(line:string) => {
             owner: owner,
             repo: repo
         });
-        console.log(prs.data.repository); //print repository info
+        //console.log(prs.data.repository); //print repository info
         if(prs.data.repository.type == 'git'){
             let requestedURL:string = prs.data.repository.url
-            var result = requestedURL.substring(requestedURL.lastIndexOf("github.com"))
-            console.log(result)
-            //need to make sure there is actually something there matching before writign
-            writeToFile(outputFile, "https://" + result, "error in writing to log file")
-            writeToFile(logFile, "Wrote npm now github link succesfully!", "error in writing to log file")
+            //console.log(requestedURL)
+            if(requestedURL.includes(githubURLMatch)){
+                var result = requestedURL.substring(requestedURL.lastIndexOf("github.com"))
+                result = result.replace('.git', emptyString)
+                //console.log(result)
+                //need to make sure there is actually something there matching before writing
+                writeToFile(outputFile, result, "error in writing to log file")
+                writeToFile(logFile, "Wrote npm now github link succesfully!: " + result, "error in writing to log file")
+            }
+            else{
+                writeToFile(logFile, "No git repository found for this package. At this time this tool only supports npm modules hosted on github repositories", "error in writing to log file")
+            }
         }
         else{
-            writeToFile(logFile, "No git repository found for this package. At this time this tool only suports github repositories", "error in writing to log file")
+            writeToFile(logFile, "No git repository found for this package. At this time this tool only supports npm modules hosted on github repositories", "error in writing to log file")
         }
                 
     }
     catch (error) {
-        writeToFile(logFile, "No git repository found for this package from the API call. At this time this tool only suports github repositories", "error in writing to log file")
+        writeToFile(logFile, "No git repository found for this package. At this time this tool only supports npm modules hosted on github repositories", "error in writing to log file")
         console.error(error)
         //return error; 
     }
@@ -79,6 +77,7 @@ function writeToFile(filename:string, lineToWrite:string, errorMessage:string){
     fs.appendFile(filename, lineToWrite + '\n', (err) => {
         if (err) {
           console.log(err);
+          console.log(errorMessage);
         }
     });
 }
@@ -86,21 +85,19 @@ function writeToFile(filename:string, lineToWrite:string, errorMessage:string){
 //PROGRAM STARTS HERE
 fs.writeFileSync(logFile, emptyString); //clear log file
 fs.writeFileSync(outputFile, emptyString); //clear url file
-console.log("in the middle")
+
 lineReader.on('line', (line) => {
     if(line.includes(npmURLMatch)){
-        writeToFile(logFile, "npm link identified", "error in writing to log file")
+        writeToFile(logFile, "npm link identified: " + line, "error in writing to log file") 
         resp(line)
     }
     else if(line.includes(githubURLMatch)){
-        writeToFile(outputFile, line, "error in writing to log file")  
-        writeToFile(logFile, "github link identified", "error in writing to log file")  
+        var result = line.substring(line.lastIndexOf("github.com"))
+        writeToFile(logFile, "github link identified: " + result, "error in writing to log file")   
+        writeToFile(outputFile, result, "error in writing to output file")  
     }
     else {
-        writeToFile(logFile, "neither link identiied, check input format. the line: " + line, "error in writing to log file")  
-    }
+        writeToFile(logFile, "neither url link identiied, check input format. provided url: " + line , "error in writing to log file")  
+    } 
     
 });
-
-console.log("near the end")
-console.log("end")
