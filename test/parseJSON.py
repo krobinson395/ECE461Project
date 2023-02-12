a97fa1d0b476af7d6b76b73e0e1d098535230dd1
Kyle <robin489@purdue.edu>
2023-02-12 12:32:32 -0500

    Added additional testing framework for jest

--- test/parseJSON.py
+++ test/parseJSON.py
@@ -10,12 +10,19 @@ fileKeys = list(files.keys())
 curFile = files.get(fileKeys[0])
 #print(curFile)
 summary = curFile.get('summary')
-covered_lines = summary.get('covered_lines')
-total_lines = summary.get('num_statements')
+covered_lines += summary.get('covered_lines')
+total_lines += summary.get('num_statements')
 
+
+
+file = open('../coverage/coverage-summary.json')
+jestData = json.load(file)
+jestTotal = jestData.get('total')
+jestStatements = jestData.get('statements')
+covered_lines += jestStatements.get('total')
+total_lines += jestStatements.get('covered')
 
 coverage = (covered_lines / total_lines) * 100
-
 inputFile = open('pythonResults.txt', 'r')
 testData = inputFile.read().splitlines()
 passed = int(testData[1]) - int(testData[0])
