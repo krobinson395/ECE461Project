0b33601b0843628a8c359eab051fe9d5c3d847cd
Kyle <robin489@purdue.edu>
2023-02-12 02:21:39 -0500

    Finished json parsing capabilities

--- test/parseJSON.py
+++ test/parseJSON.py
@@ -3,12 +3,18 @@ import json
 file = open('coverage.json')
 
 data = json.load(file)
+#print(data.keys())
+files = data.get('files')
+fileKeys = list(files.keys())
+#print(files)
+curFile = files.get(fileKeys[0])
+#print(curFile)
+summary = curFile.get('summary')
+covered_lines = summary.get('covered_lines')
+total_lines = summary.get('num_statements')
 
-covered_lines = data.get('summary').get('covered_lines')
-total_lines = data.get('summary').get('num_statements')
 
-
-coverage = covered_lines / total_lines
+coverage = (covered_lines / total_lines) * 100
 
 print(f'Test Coverage {coverage}%');
 
